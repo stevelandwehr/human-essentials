a948571772b7e24e59297384a1be2b74193482d2
exbinary <exbinary@gmail.com>
2021-10-07 14:01:00 -0400

    Add spec scenario for consolidated login with both bank/partner accounts
    Co-authored-by: patmccler <patmccler@gmail.com>

--- spec/system/consolidated_login_system_spec.rb
+++ spec/system/consolidated_login_system_spec.rb
@@ -48,4 +48,30 @@ RSpec.describe "Consolidated login with email lookup", type: :system, js: true d
       expect(page).to have_current_path "/partners/dashboard"
     end
   end
+
+  context "user with both partner and bank logins" do
+    before do
+      user.organization.update!(name: "A Bank")
+      partner_user.partner.update!(name: "A Partner")
+      partner_user.update!(email: user.email)
+
+      fill_in "Email", with: user.email
+      click_button "Continue"
+    end
+
+    it "presents a dropdown allowing the user to pick which org they'd like to log in for and prefills their email" do
+      expect(page.find_field("Email").value).to eql(user.email)
+      expect(page).to have_select("user[organization]", options: ["A Bank", "A Partner"])
+    end
+
+    it "logs in based on the organization selected from the dropdown" do
+      select "A Partner", from: "user[organization]"
+      click_button "Continue"
+
+      fill_in "Password", with: user.password
+      click_button "Log in"
+
+      expect(page).to have_current_path "/partners/dashboard"
+    end
+  end
 end
